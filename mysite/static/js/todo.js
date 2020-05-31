40354e728d34da6aaa9ba98601c18702d169fb14
Fatma Akram <fatma.khater96@gmail.com>
2020-06-01 01:12:32 +0200

    Update create new todo task ajax

--- mysite/static/js/todo.js
+++ mysite/static/js/todo.js
@@ -1,7 +1,40 @@
-function toggleMark(obj,id){
+$(document).on("submit", "form", function (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    let form = $(this);
+    let url = form.attr('action');
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: {
+            title: $('input[type=text]').val(),
+            csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
+        },
+        success: function (item) {
+            let newTask = `
+                <li class="list-group-item">
+                    <label>
+                    <input type="checkbox"
+                           onchange='toggleMark(this,${ item['id'] })'>
+                    ${ item['title'] }
+                    </label>
+                 <span  class="float-right text-danger"
+                        onclick='deleteTask(this,${ item['id'] })'>X</span>
+                </li>`
+            $('ul').append(newTask);
+            $('.no-tasks').hide()
+            $('input[type=text]').val('')
+        },
+        error: function (error) {
+            throw new Error(`Error!: ${error['statusText']}`);
+        }
+    });
+});
+
+function toggleMark(obj, id) {
     clickedTask = $(obj).parents('label');
     textDecoration = clickedTask.css('text-decoration-line');
-    reqType = (textDecoration === 'line-through') ?  'uncomplete' : 'complete'
+    reqType = (textDecoration === 'line-through') ? 'uncomplete' : 'complete'
     $.ajax({
         type: 'POST',
         url: `/todos/${reqType}`,
@@ -17,7 +50,8 @@ function toggleMark(obj,id){
         }
     })
 }
-function deleteTask(obj,id){
+
+function deleteTask(obj, id) {
     clickedTask = $(obj).parents('li');
     $.ajax({
         type: 'POST',
@@ -28,7 +62,7 @@ function deleteTask(obj,id){
         },
         success: function (response) {
             clickedTask.remove();
-           $('li').length === 0 ? $('.no-tasks').show():'';
+            $('li').length === 0 ? $('.no-tasks').show() : '';
         },
         error: function (error) {
             throw new Error(`Error!: ${error['statusText']}`);
--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, JsonResponse
-
+from django.http import  JsonResponse
 from .models import Todo
 
 
@@ -10,16 +9,18 @@ def index(request):
 
 
 def create(request):
-    new_item = Todo(title = request.POST['title'])
-    new_item.save()
-    return HttpResponseRedirect('/todos/')
+    item = Todo.objects.create(title = request.POST['title'])
+    return JsonResponse({
+        'id': item.id,
+        'title': item.title,
+        'completed': item.completed
+    }, status=200)
 
 
 def mark_complete(request):
     if request.is_ajax and request.method == "POST":
         item = Todo.objects.filter(id=request.POST['id']).update(completed=True)
         return JsonResponse({"success": "true"}, status=200)
-
     # some error occured
     return JsonResponse({"error": ""}, status=400)
 
@@ -28,8 +29,6 @@ def mark_uncomplete(request):
     if request.is_ajax and request.method == "POST":
         item = Todo.objects.filter(id=request.POST['id']).update(completed=False)
         return JsonResponse({"success": "true"}, status=200)
-
-    # some error occured
     return JsonResponse({"error": ""}, status=400)
 
 
@@ -37,6 +36,4 @@ def delete(request):
     if request.is_ajax and request.method == "POST":
         item = Todo.objects.filter(id=request.POST['id']).delete()
         return JsonResponse({"success": "true"}, status=200)
-
-    # some error occured
     return JsonResponse({"error": ""}, status=400)
